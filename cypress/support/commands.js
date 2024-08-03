bffc6fdeb82635fa6e8e737dec38c5933e78c8a4
g4ss4n <g4ss4n@gmail.com>
2024-08-03 15:49:41 +0300

    Cypress completed

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,35 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// cypress/support/commands.js
+
+// Command to visit the login page
+Cypress.Commands.add('visitLoginPage', (url) => {
+    cy.visit(url);
+});
+
+// Command to perform login
+Cypress.Commands.add('performLogin', (username, password) => {
+    cy.get('#username').type(username);
+    cy.get('#password').type(password);
+    cy.get('#submit').click();
+});
+
+// Command to verify successful login
+Cypress.Commands.add('verifySuccessfulLogin', () => {
+    cy.url().should('include', '/logged-in-successfully/');
+    cy.get('.post-title').should('contain', 'Logged In Successfully');
+});
+
+// Command for page overview
+Cypress.Commands.add('pageOverview', () => {
+    cy.get('nav').should('contain', 'Home');
+    cy.get('nav').should('contain', 'Practice');
+    cy.get('nav').should('contain', 'Courses');
+    cy.get('nav').should('contain', 'Blog');
+    cy.get('nav').should('contain', 'Contact');
+});
+
+// Command to perform logout
+Cypress.Commands.add('performLogout', () => {
+    cy.contains('Log out').click();
+    cy.url().should('include', '/practice-test-login/');
+    cy.get('h2').should('contain', 'Test login');
+});

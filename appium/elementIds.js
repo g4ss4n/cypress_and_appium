0568fd8408f78a45d17432dde85212743eb3cf5c
g4ss4n <g4ss4n@gmail.com>
2024-08-05 23:29:14 +0300

    Modify Appium Example

---
+++ appium/elementIds.js
@@ -0,0 +1,6 @@
+// elementIds.js
+module.exports = {
+  titleId: 'cm.aptoide.pt:id/title',
+  descriptionId: 'cm.aptoide.pt:id/description',
+  skipButtonId: 'cm.aptoide.pt:id/skip_button'
+};
--- appium/tests/sanity.js
+++ appium/tests/sanity.js
@@ -1,31 +1,25 @@
 const { remote } = require('webdriverio');
 const commands = require('../commands');
 const config = require('../config');
+const elementIds = require('../elementIds');
 
 async function runTest() {
   const driver = await remote({ ...config.wdOpts, capabilities: config.capabilities });
 
   try {
-//    await commands.allowPermissions(driver);
+    await commands.allowPermissions(driver);
+    await commands.validateElementText(driver, elementIds.titleId, 'Discover the Best Apps');
+    await commands.validateElementText(driver, elementIds.descriptionId, 'No content restrictions for you to find top apps');
+    await commands.validateElementText(driver, elementIds.skipButtonId, 'SKIP');
+    await commands.clickElementById(driver, elementIds.skipButtonId);
 
-    await commands.validateElementText(driver, 'cm.aptoide.pt:id/title', 'Discover the Best Apps');
-
-    await commands.validateElementText(driver, 'cm.aptoide.pt:id/description', 'No content restrictions for you to find top apps');
-
-    await commands.validateElementText(driver, 'cm.aptoide.pt:id/skip_button', 'SKIP');
-
-    await commands.clickElementById(driver, 'cm.aptoide.pt:id/skip_button');
-
-    commands.printSuccess();
     await driver.pause(10000);
+
   } catch (error) {
     console.error('Test failed:', error.message);
-    const failedCommand = commands.extractFailedCommand(error.stack);
-    if (failedCommand) {
-      console.error('Failed command:', failedCommand);
-    }
     await commands.stopAppiumServer();
     process.exit(1);
+
   } finally {
     await driver.pause(1000);
     await driver.deleteSession();
